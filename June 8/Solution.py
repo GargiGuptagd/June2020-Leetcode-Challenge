bb0b19d6985c99011d45ba138b8af1d0f833768d
GargiGuptagd <47682663+GargiGuptagd@users.noreply.github.com>
2020-06-08 09:29:53 -0700

    update commit

--- June 8/Solution.py
+++ June 8/Solution.py
@@ -2,14 +2,12 @@ import math
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
         i=0
-        while True:
-	    i=0
-            while True:
-                num=math.pow(2,i)
-                if n==num:
-                    return True
-                if num>n:
-                    return False
-                i+=1
+       	while True:
+           num=math.pow(2,i)
+           if n==num:
+               return True
+           if num>n:
+               return False
+           i+=1
         
        
